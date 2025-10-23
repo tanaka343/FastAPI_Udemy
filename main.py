33acb43542af24fd73f13290a4c590907e5f168b
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-23 15:30:15 +0900

    READ操作学習

--- main.py
+++ main.py
@@ -8,8 +8,30 @@ items = [
   {"id":3,"name":"tanaka3"},
 ]
 
-@app.get("/{id}")
-async def top(id: int):
-  for item in items:
-    if item["id"]==id:
-        return item["name"]
+#READ処理
+
+@app.get("/items")
+async def find_all():
+  return items
+
+@app.get("/items/{id}")
+async def find_by_id(id :int):
+    for item in items:
+       if item["id"]==id:
+          return item
+
+##nameすべて一致     
+@app.get("/items/")
+async def find_by_name(name :str):
+   for item in items:
+      if item["name"]==name:
+         return item
+
+##name一部一致
+@app.get("/items//")
+async def find_by_name2(name :str):
+    filtered_name = []
+    for item in items:
+      if name in item["name"]:
+         filtered_name.append(item)
+    return filtered_name
