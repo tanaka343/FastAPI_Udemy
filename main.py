bf889aa1c2ce64389635601235faeb46374ba204
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-23 14:35:12 +0900

    api基本操作学習

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-items =[
-    {"id":1,"name":"tanaka"},
-    {"id":2,"name":"satou"},
+items = [
+  {"id":1,"name":"tanaka1"},
+  {"id":2,"name":"tanaka2"},
+  {"id":3,"name":"tanaka3"},
 ]
 
-
-@app.get("/")
-async def sanple(items):
-    return items
+@app.get("/{id}")
+async def top(id: int):
+  for item in items:
+    if item["id"]==id:
+        return item["name"]
---
+++ pl.py
@@ -0,0 +1,12 @@
+persons = [
+  {"id":1,"name":"tanaka1"},
+  {"id":2,"name":"tanaka2"},
+  {"id":3,"name":"tanaka3"},
+]
+
+# for person in persons:
+#   for i,v in person.items():
+#     print(i,v)
+
+for person in persons:
+  print(person["name"])
