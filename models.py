f233d6f7b3622ca40fd8adbf1b12997b0c7f7d16
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-25 23:03:21 +0900

    データモデル作成

---
+++ models.py
@@ -0,0 +1,8 @@
+from database import Base
+from sqlalchemy import Column,Integer,String
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer,primary_key=True)
+    name = Column(String,nullable=False)
+    email = Column(String,nullable=True)
