b906b79c036eec12d483dedcf8809d586bdcc691
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-26 18:42:20 +0900

    バリデーション作成

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-from fastapi import FastAPI,Body
+from fastapi import FastAPI,Body,Depends
+from schemas import ItemCreate,ItemUpdate,ItemResponse
+from database import get_data
+from typing import Optional
 
 app = FastAPI()
 
 items = [
-  {"id":1,"name":"tanaka1"},
-  {"id":2,"name":"tanaka2"},
-  {"id":3,"name":"tanaka3"},
+  {"id":1,"name":"tanaka1","email":"nhkh@com"},
+  {"id":2,"name":"tanaka2","email":"nhkh@com"},
+  {"id":3,"name":"tanaka3","email":"nhkh@com"},
 ]
 
 #READ処理
 
-@app.get("/items")
+@app.get("/items",response_model=list[ItemResponse])
 async def find_all():
     return items
 
 
-@app.get("/items/{id}")
+@app.get("/items/{id}",response_model=Optional[ItemResponse])
 async def find_by_id(id :int):
     for item in items:
         if item["id"]==id:
             return item
         
 
-@app.get("/items/")
+@app.get("/items/",response_model=list[ItemResponse])
 async def find_by_name(name :str):
     filtered_name=[]
     for item in items:
@@ -30,24 +33,28 @@ async def find_by_name(name :str):
             filtered_name.append(item)
     return filtered_name
 
-@app.post("/items")
-async def create(create_item=Body()):
+
+
+@app.post("/items",response_model=ItemResponse)
+async def create(create_item :ItemCreate):
     new_item={
         "id":len(items)+1,
-        "name":create_item["name"],
+        "name":create_item.name,
+        "email":create_item.email,
     }
     items.append(new_item)
     return new_item
 
-@app.put("/items/{id}")
-async def update(id :int,update_item=Body()):
+@app.put("/items/{id}",response_model=Optional[ItemResponse])
+async def update(id :int,update_item :ItemUpdate):
     for item in items:
         if item["id"]==id:
-            item["name"]=update_item["name"]
+            item["name"]=item["name"] if update_item.name is None else update_item.name
+            item["email"]=item["email"] if update_item.email is None else update_item.email
 
             return item
         
-@app.delete("/items/{id}")
+@app.delete("/items/{id}",response_model=Optional[ItemResponse])
 async def deleate(id :int):
     for i in range(len(items)):
         if items[i]["id"]==id:
--- schemas.py
+++ schemas.py
@@ -1,5 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
+from typing import Optional
 
 
-class create_item(BaseModel):
-    
+class ItemCreate(BaseModel):
+    name :str = Field(min_length=2,max_length=20,examples=["tanaka"])
+    email :str = Field(examples=["email@com"])
+
+
+class ItemUpdate(BaseModel):
+    name : Optional[str] = Field(default=None,min_length=2,max_length=20,examples=["yamada"])
+    email : Optional[str] = Field(default=None,examples=["email@com"])
+
+
+class ItemResponse(BaseModel):
+    name :str = Field(min_length=2,max_length=20,examples=["satou"])
+    email :str = Field(min_length=2,max_length=20,examples=["dafgz@com"])
