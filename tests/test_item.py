4fea5231d3415f12e5d34d274242a50f0948bc3a
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-11-03 16:02:56 +0900

    テストケースを追加し、アイテムの取得、作成、更新、削除の正常系および異常系を検証

---
+++ tests/test_item.py
@@ -0,0 +1,59 @@
+from fastapi.testclient import TestClient
+
+def test_find_all(client_fixture :TestClient):
+    response = client_fixture.get("/items")
+    assert response.status_code == 200
+    items = response.json()
+    assert len(items) == 2
+
+def test_find_by_id_正常系(client_fixture :TestClient):
+    response = client_fixture.get("/items/1")
+    assert response.status_code == 200
+    items = response.json()
+    assert items["id"] == 1
+
+def test_find_by_id_異常系(client_fixture :TestClient):
+    response = client_fixture.get("/items/10")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Item not found"
+
+def test_find_by_name(client_fixture :TestClient):
+    response = client_fixture.get("/items/?name=tanaka")
+    assert response.status_code == 200
+    item = response.json()
+    assert len(item) == 1
+    assert item[0]["name"] == "tanaka"
+
+def test_create(client_fixture :TestClient):
+    response = client_fixture.post("/items",json={"name":"suzuki","email":"hwou@com"})
+    assert response.status_code == 201
+    item = response.json()
+    assert item["id"] == 3
+    assert item["name"] == "suzuki"
+    assert item["email"] == "hwou@com"
+
+    response = client_fixture.get("/items")
+    assert len(response.json()) == 3
+
+def test_update_正常系(client_fixture :TestClient):
+    response = client_fixture.put("/items/1",json={"name":"tanaka2"})
+    assert response.status_code == 200
+    item = response.json()
+    assert item["name"] == "tanaka2"
+
+def test_update_異常系(client_fixture :TestClient):
+    response = client_fixture.put("/items/10",json={"name":"tanaka2"})
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Item not found"
+
+def test_delete_正常系(client_fixture :TestClient):
+    response = client_fixture.delete("/items/1")
+    assert response.status_code == 200
+    item = response.json()
+    response = client_fixture.get("/items")
+    assert len(response.json()) == 1
+
+def test_delete_異常系(client_fixture :TestClient):
+    response = client_fixture.delete("/items/10")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Item not found"
